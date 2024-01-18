4672fc19f037f4356da9394348932d033d4213fa
Navaiklam <dmlk.dark@gmail.com>
2024-01-18 01:42:50 -0300

    add dist7

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  buildOptions: {
-    out: "dist",
-  },
     site: 'https://navaiklam.github.io/',
     base: '/CV',
   integrations: [tailwind()],
