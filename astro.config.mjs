668753604f05d0b461aefc46ec597302282ce0e3
Navaiklam <dmlk.dark@gmail.com>
2024-01-18 00:29:17 -0300

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Navaiklam.github.io',
+  site: 'https://navaiklam.github.io',
   integrations: [tailwind()]
 });
