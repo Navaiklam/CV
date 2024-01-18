8145412ae609c8c75c67283a9022bdf6bd9ff918
Navaiklam <dmlk.dark@gmail.com>
2024-01-18 01:02:23 -0300

    add dist a config.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  buildOptions: {
+    // Configura la carpeta de salida
+    out: "dist",
+  },
   site: 'https://navaiklam.github.io',
   integrations: [tailwind()]
 });
